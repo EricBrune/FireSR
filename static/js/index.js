13e6c59734741c94f5910e99c8322867c1c53581
Eric Brune <ericjosefbrune@gmail.com>
2024-05-29 20:19:47 +0200

    Added slider

--- static/js/index.js
+++ static/js/index.js
@@ -19,3 +19,8 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+const container = document.querySelector('.container');
+document.querySelector('.slider').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%`);
+});
