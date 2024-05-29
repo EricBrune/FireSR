9c7e2d3bd89dca0253352304004b366e06100b0e
Eric Brune <ericjosefbrune@gmail.com>
2024-05-29 20:41:05 +0200

    Revert bad changes and add bulma-slider

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,21 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
+
     var options = {
-      slidesToScroll: 1,
-      slidesToShow: 1,
-      loop: true,
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 5000,
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 5000,
     }
 
-    // Initialize all div with carousel class
+		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-
+	
     bulmaSlider.attach();
-});
 
-const container = document.querySelector('.container');
-document.querySelector('.slider').addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value}%`);
-});
+})
