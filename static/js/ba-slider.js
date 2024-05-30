b996747a93f76f857301a540fac5da256ad14cf9
Eric Brune <ericjosefbrune@gmail.com>
2024-05-30 09:48:35 +0200

    Slider fixes

---
+++ static/js/ba-slider.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const slider = document.querySelector('.slider');
+  
+    slider.addEventListener('input', (e) => {
+      container.style.setProperty('--position', `${e.target.value}%`);
+    });
+  });
+  
